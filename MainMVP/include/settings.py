b2ee9825bf6e969e0deb9ec187227f28a3983de5
Federico Zappaterra <53621597+fzappate@users.noreply.github.com>
2024-09-15 11:29:34 -0500

    Cleaned up settings.py

--- MainMVP/include/settings.py
+++ MainMVP/include/settings.py
@@ -1,12 +1,6 @@
 class Settings():
     '''Class containing all the settings of the software instance.'''
 
-    # Slots 
-    # __slots__ = ('inputsFileName','exePath','profile1Name','profile2Name',
-    #              'geomCodeGearParaDict','radialRecessDictionary',
-    #              'workingFolder',
-    #              'inputsFilePath','profile1Path','profile2Path')
-
     def __init__(self)->None:
         '''Initialize the application setting object.'''
 
@@ -14,23 +8,3 @@ class Settings():
         self.workingFolder = ''
         self.resultsFile = ''
         self.resultsFilePath = ''
-        
-
-        # Predefined Gear Generator files name
-        self.gearGenInputsFileName = '/shapingPara_dim.txt'
-        self.exePath = './MainMVP/gearOpti.exe'
-        self.profile1Name = '/gear1.txt'
-        self.profile2Name = '/gear2.txt'
-        
-        self.gearGenInputsFilePath = ''
-        self.profile1Path = ''
-        self.profile2Path = ''
-
-        # Predefined Gear Generator files name
-        self.geomCodeInputsFileName = '/inputs.txt'
-        self.geomCodeGearParaDict = '/Inputs/gearParametersDictionary.txt'
-        self.radialRecessDict = '/Inputs/radialRecessDictionary.txt'
-
-        self.geomCodeInputsFilePath = ''
-        self.geomCodeGearParaDictPath = ''
-        self.radialRecessDictPath = ''
