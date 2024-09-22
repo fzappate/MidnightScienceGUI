bed779500aebb9c4b03462b37f6958397c21c378
Federico Zappaterra <53621597+fzappate@users.noreply.github.com>
2024-09-22 11:22:13 -0500

    Improved ScrolResizeFrame code layout improved

--- ui/resizableframe.py
+++ ui/resizableframe.py
@@ -154,24 +154,25 @@ class ResizeScrollVFrameRightEdge(ResizableFrameRightEdge):
         self.interior_id = self.canvas.create_window(0, 0, window=self.interior, anchor='nw')
 
         # Bind the frame to the scrollbar so that it can be scrolled while paning over it
-        # self.interior.bind("<Configure>",lambda e: self.canvas.configure(scrollregion=self.interior.bbox("all")))
         self.bind('<Enter>', self.boundToMouseWheel)
         self.bind('<Leave>', self.unboundToMouseWheel)
         
-        def _configure_interior(event):
-            # Update the scrollbars to match the size of the inner frame.
-            size = (self.interior.winfo_reqwidth(), self.interior.winfo_reqheight())
-            self.canvas.config(scrollregion="0 0 %s %s" % size)
-            if self.interior.winfo_reqwidth() != self.canvas.winfo_width():
-                # Update the canvas's width to fit the inner frame.
-                self.canvas.config(width=self.interior.winfo_reqwidth())
-        self.interior.bind('<Configure>', _configure_interior)
-
-        def _configure_canvas(event):
-            if self.interior.winfo_reqwidth() != self.canvas.winfo_width():
-                # Update the inner frame's width to fill the canvas.
-                self.canvas.itemconfigure(self.interior_id, width=self.canvas.winfo_width())
-        self.canvas.bind('<Configure>', _configure_canvas)
+        # Bind canvas to frame and viceversa
+        self.interior.bind('<Configure>', self._configure_interior)
+        self.canvas.bind('<Configure>', self._configure_canvas)
+        
+    def _configure_interior(self,event):
+        # Update the scrollbars to match the size of the inner frame.
+        size = (self.interior.winfo_reqwidth(), self.interior.winfo_reqheight())
+        self.canvas.config(scrollregion="0 0 %s %s" % size)
+        if self.interior.winfo_reqwidth() != self.canvas.winfo_width():
+            # Update the canvas's width to fit the inner frame.
+            self.canvas.config(width=self.interior.winfo_reqwidth())
+        
+    def _configure_canvas(self,event):
+        if self.interior.winfo_reqwidth() != self.canvas.winfo_width():
+            # Update the inner frame's width to fill the canvas.
+            self.canvas.itemconfigure(self.interior_id, width=self.canvas.winfo_width())
         
     def boundToMouseWheel(self, event):
         self.canvas.bind_all("<MouseWheel>", self._on_mousewheel)
