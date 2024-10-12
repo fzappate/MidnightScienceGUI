d17f1764cd7b1ee3c5632178e0f3e7edccf4d50b
Federico Zappaterra <53621597+fzappate@users.noreply.github.com>
2024-10-12 17:32:05 -0500

    Improved PlottedSignal class

--- model/PlottedSignal.py
+++ model/PlottedSignal.py
@@ -1,9 +1,22 @@
-class PlottedSignals():
-    def __init__(self)->None:
+from model.Signal import Signal
+
+class PlottedSignals(Signal):
+    def __init__(self,
+                 lineThickness = 1,
+                 lineStyle = '-',
+                 marker = '',
+                 color = [1, 1, 1],
+                 *args,
+                 **kwargs)->None:
         '''
         Class that generates the model of a PlottedSignals object.
         The purpose of this class is to store all the signals
         and related information contained in a subplot.
         '''
-        # Dictionary with signal names and values
-        self.signals = {}
+        super().__init__(*args,**kwargs)
+        
+        self.lineThickness = lineThickness 
+        self.lineStyle = lineStyle
+        self.marker = marker
+        self.color = color
+        
